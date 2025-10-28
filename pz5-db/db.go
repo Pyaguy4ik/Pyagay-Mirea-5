fbcd638a78ef0ed1fee4b234a8bc94c4b31c4c3a
Pyaguy4ik <pyagaydan22@gmail.com>
2025-10-28 16:55:07 +0300

    Create db.go

---
+++ pz5-db/db.go
@@ -0,0 +1,33 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func OpenDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// Настройки пула соединений
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(5)
+	db.SetConnMaxLifetime(30 * time.Minute)
+
+	// Проверка соединения с таймаутом
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	log.Println("Connected to PostgreSQL")
+	return db, nil
+}
